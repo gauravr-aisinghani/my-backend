a3d5e5ba7646e8c6bc440ce1e106235f31942071
Ritik <ritikraisinghani5@gmail.com>
2025-12-20 18:41:40 +0530

    add to final logic

--- src/main/java/com/example/entity/TransporterVehicle.java
+++ src/main/java/com/example/entity/TransporterVehicle.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "transporter_vehicle")
+@Table(name = "yfs_transporter_vehicle_details")
 public class TransporterVehicle {
 
     @Id
